816fecfbb25948a7b8c1ed4db20fa7d0e3fff402
John Assebe <johnassebe@gmail.com>
2023-12-17 00:50:34 +0300

    scroll to top when path changes :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 /* eslint-disable */
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Layout from "./components/layout";
 import FilteredCountries from "./components/filteredCountries";
 import CountryDetail from "./components/countryDetail";
 const client = new QueryClient();
+const ScrollTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 function App() {
   return (
     <QueryClientProvider client={client}>
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<FilteredCountries />} />
